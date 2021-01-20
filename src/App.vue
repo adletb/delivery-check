6aeae2ba6d5dc66d40c43dd5378c2e90586aa41d
Adlet Bakyt <bakyt_adlet@mail.ru>
2021-01-20 15:33:43 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="deliveryListContainer">
+    <div class="heading">
+      <h2 id="title">Available delivery</h2>      
+    </div>  
+    <div> 
+      <span class="item"> Select city</span>
+      <select name="check city" id="" 
+        @change="onChange($event)" v-model="key">
+        <option selected value="nur-sultan">Нур-Султан</option>
+        <option value="almaty">Алматы</option>
+        <option value="semey">Семей</option>
+        <option value="aktobe">Актобе</option>
+      </select>
+    </div>
+    <div class="item">
+      <span>City</span>
+      <span>Type</span>
+      <span>Price</span>
+      <span>Available</span>
+    </div>
+    <hr> 
+    <list-view :items="items"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import listView from './components/listView.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    listView
+    
+  },
+  data(){
+    return {
+      items: [],
+      key: "",
+      url: `http://localhost:3000/delivery/check?search=`,
+      city: 'nur-sultan'
+    }
+  },
+  methods: {
+    onChange(event) {            
+            this.city = event.target.value
+            console.log(this.city)
+            this.getList()
+    },
+    getList(){
+      axios.get(this.url+this.city)
+      .then( response => {
+        this.items = response.data
+        console.log(this.items)
+      })
+      .catch( error => {
+        console.log( error );
+      })
+    }    
+  },
+  created(){
+    this.getList()
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+ .deliveryListContainer{
+        width: 450px;
+        margin: auto;
+  }
+  .heading {
+      background: #e6e6e6;
+      padding: 10px;
+  }
+  #title {
+      text-align: center;
+  }
+  .item {
+    background: #e6e6e6;
+    font-weight: bold;
+    padding: 5px;
+    margin-top: 5px;
+  }
+  span {
+    padding-right: 80px;
+  }  
+  select {
+    margin: 10px 0;
+    width: 200px;
+    /* border-radius: 5px; */
+    height: 28px;
+  }
 </style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="item" :class="[!item.available ? 'completed' : '']">     
+      <span class="capital"> {{ item.city }} </span>
+      <span> {{ item.type }} </span>
+      <span> {{ item.price }} </span>
+      <span v-if="item.available"> Yes </span>
+      <span v-else > No </span>
+   </div>
+</template>
+
+<script>
+export default {
+    props: ['item']
+}
+</script>
+
+<style scoped>
+    .completed{
+        text-decoration: line-through;
+        color: #999999;
+    }
+    .itemText {
+        width: 100%;
+        margin-left: 20px;
+    }
+    .item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .trashcam{
+        background: #e6e6e6;
+        background: none;
+        color: #FF0000;
+        outline: none;
+    }
+    span {
+       width: 120px;
+    }
+    .capital{
+    text-transform: capitalize;
+    }  
+</style>
